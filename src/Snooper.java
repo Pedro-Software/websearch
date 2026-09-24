993c350b44199a6f5dcba012b30d58ba05027b32
Pedro Henrique Borges Silva <engpedro.run@gmail.com>
2026-09-23 23:33:48 -0300

    Atualiza Snooper para testar diferentes estratégias

--- src/Snooper.java
+++ src/Snooper.java
@@ -1,5 +1,6 @@
 public class Snooper {
     public Snooper(WebSearchModel model) {
-        model.addQueryObserver(query -> System.out.println("Query: " + query));
+        model.addQueryObserver(query -> System.out.println("Oh Yes! " + query), new FriendFilter());
+        model.addQueryObserver(query -> System.out.println("So long " + query), new LongQueryFilter());
     }
 }
